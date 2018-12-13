0bb78d5e6de2e529ddf279003e2c2eea94b73b5a
JamesChu <empirejames@hotmail.com>
2018-12-13 09:59:01 +0800

    Fix dog and cat would not display pin in map

--- AnimalsNeedHome/MapViewController.swift
+++ AnimalsNeedHome/MapViewController.swift
@@ -20,16 +20,18 @@ class MapViewController: UIViewController, MKMapViewDelegate{
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("Get Type \(stringArray[0])" )
         mainMapview.delegate = self
+        
         if(stringArray[0] == "3"){
             latitude = 25.084820
             longitude = 121.560433
             location = "愛兔協會"
         }else{
-           latitude = 25.084820
+            latitude = 25.0631109
+            longitude = 121.6067468
             location = "台北市動物之家"
         }
+        
         let sourceLocation = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
       
         let sourcePlacemark = MKPlacemark(coordinate: sourceLocation, addressDictionary: nil)
@@ -39,10 +41,19 @@ class MapViewController: UIViewController, MKMapViewDelegate{
         let sourceAnnotation = MKPointAnnotation()
         sourceAnnotation.title = location
         
+
+        
+        
         if let location = sourcePlacemark.location {
             sourceAnnotation.coordinate = location.coordinate
+            self.mainMapview.addAnnotation(sourceAnnotation)
         }
+      
         self.mainMapview.showAnnotations([sourceAnnotation], animated: true )
+        self.mainMapview.showsCompass = true
+        self.mainMapview.showsScale = true
+        self.mainMapview.showsTraffic = true
+        
         let directionRequest = MKDirectionsRequest()
         directionRequest.source = sourceMapItem
 
